ad912acdfaab46b4ad0518551a4e81135634f725
Blak3Nick <blaken2021@gmail.com>
2017-03-17 12:31:00 -0400

    Created a new DB for the model

--- CustomerManager/CustomerManager/Controllers/CustomersController.cs
+++ CustomerManager/CustomerManager/Controllers/CustomersController.cs
@@ -4,16 +4,43 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CustomerManager.Models;
+using CustomerManager.ViewModel;
+using Microsoft.Ajax.Utilities;
 
 namespace CustomerManager.Controllers
 {
     public class CustomersController : Controller
     {
         // GET: Customers
-        public ActionResult Index()
+        public ActionResult Index(int? pageIndex, string sortBy)
         {
-            var customer = new Customer() {Name = "Caleb Thompson"};
-            return View(customer);
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Cust1"},
+                new Customer {Name = "Cust2"}
+            };
+
+            var viewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+
+        }
+        [Route("customers/name/name")]
+        public ActionResult ByName(string name)
+        {
+
+            return View();
         }
     }
 }
---
+++ CustomerManager/CustomerManager/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CustomerManager.Models;
+
+namespace CustomerManager.ViewModel
+{
+    public class CustomerViewModel
+    {
+        public Customer Customer { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
