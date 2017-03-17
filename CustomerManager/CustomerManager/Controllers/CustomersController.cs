50002f78a0187b2cca8cbbfb1ab8cc7d6191aedc
Blak3Nick <blaken2021@gmail.com>
2017-03-17 14:32:16 -0400

    Added new methods for accessing data

--- CustomerManager/CustomerManager/Controllers/CustomersController.cs
+++ CustomerManager/CustomerManager/Controllers/CustomersController.cs
@@ -39,5 +39,44 @@ namespace CustomerManager.Controllers
 
             return View(customer);
         }
+        public ActionResult New()
+        {
+            var customers = _context.Customers.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                Customers = customers
+            };
+            return View("CustomerForm", viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Email = customer.Email;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+            };
+
+            return View("CustomerForm", viewModel);
+        }
     }
 }
---
+++ CustomerManager/CustomerManager/ViewModel/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CustomerManager.Models;
+
+namespace CustomerManager.ViewModel
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<Customer> Customers { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
